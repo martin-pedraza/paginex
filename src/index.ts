2b203d954191b2ee0a14573f5eae438d35f74796
martin-pedraza <pedraza.martindiego@gmail.com>
2026-01-01 09:54:33 -0300

    0.1.2 : fix module export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { paginate } from './paginate';
 export { Paginex } from './Paginex';
 export { PaginationResult, PaginationMeta } from './types';
-
+export { paginate };
 export default paginate;
